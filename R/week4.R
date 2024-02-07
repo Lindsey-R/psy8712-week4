4865342c3f1fb982cc3d4c63fdb204e405795f37
Lindsey-R <105560190+Lindsey-R@users.noreply.github.com>
2024-02-07 16:53:43 -0600

    Change Project Structures

--- R/week4.R
+++ R/week4.R
@@ -3,7 +3,7 @@ setwd(dirname(rstudioapi::getActiveDocumentContext()$path))
 library(tidyverse)
 
 # Data Import
-import_tbl <- read_delim("week4.dat", delim = "-", col_names = c("casenum", "parnum","stimver", "datadate", "qs"))
+import_tbl <- read_delim("../data/week4.dat", delim = "-", col_names = c("casenum", "parnum","stimver", "datadate", "qs"))
 str(import_tbl)
 wide_tbl <- separate(import_tbl, qs, into = c("q1","q2","q3","q4","q5"), sep = "-")
 wide_tbl[,5:9] <- sapply(wide_tbl[,5:9], as.integer)
