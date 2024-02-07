7d4f8ef6fca91856f861f9fcbe19ded4d0617d54
Lindsey-R <105560190+Lindsey-R@users.noreply.github.com>
2024-02-07 16:48:35 -0600

    Finish part 2 (1st version

--- week4.R
+++ week4.R
@@ -1,3 +1,13 @@
 # Script Settings and Resources
 setwd(dirname(rstudioapi::getActiveDocumentContext()$path))
 library(tidyverse)
+
+# Data Import
+import_tbl <- read_delim("week4.dat", delim = "-", col_names = c("casenum", "parnum","stimver", "datadate", "qs"))
+str(import_tbl)
+wide_tbl <- separate(import_tbl, qs, into = c("q1","q2","q3","q4","q5"), sep = "-")
+wide_tbl[,5:9] <- sapply(wide_tbl[,5:9], as.integer)
+wide_tbl$datadate <- mdy_hms(wide_tbl$datadate) #Using lubridate
+wide_tbl[,5:9] <- replace(wide_tbl[,5:9], wide_tbl[,5:9] == 0, NA)
+wide_tbl <- wide_tbl[is.na(wide_tbl$q2) == FALSE, ]
+long_tbl <- pivot_longer(wide_tbl, cols = c('q1','q2','q3','q4','q5'), names_to = "question", values_to = "response")
