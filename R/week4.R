2228a77fa5de676d480fc3c39890c4d2350ddf67
Lindsey-R <105560190+Lindsey-R@users.noreply.github.com>
2024-02-07 17:05:44 -0600

    changed line 12

--- R/week4.R
+++ R/week4.R
@@ -9,5 +9,5 @@ wide_tbl <- separate(import_tbl, qs, into = c("q1","q2","q3","q4","q5"), sep = "
 wide_tbl[,5:9] <- sapply(wide_tbl[,5:9], as.integer)
 wide_tbl$datadate <- mdy_hms(wide_tbl$datadate) #Using lubridate
 wide_tbl[,5:9] <- replace(wide_tbl[,5:9], wide_tbl[,5:9] == 0, NA)
-wide_tbl <- wide_tbl[is.na(wide_tbl$q2) == FALSE, ]
+wide_tbl <- drop_na(wide_tbl, q2)
 long_tbl <- pivot_longer(wide_tbl, cols = c('q1','q2','q3','q4','q5'), names_to = "question", values_to = "response")
